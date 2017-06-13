c9ae7e375efdb38c7ec2a3bdcab3270f3455c068
Scott Andrechek <BranchofLight@users.noreply.github.com>
2017-06-13 15:36:15 -0400

    Add files via upload

--- js/mask.js
+++ js/mask.js
@@ -1,9 +1,10 @@
-// ? used to denote repetition of part (eg. $!0?)
+// ? used to denote repetition of part (eg. $0?)
 var rep_char = '?';
 var alpha_char = 'X';
 var num_char = '#';
 var white_char = '_';
 var escape_char = '\\';
+var deletes = ["Backspace", "Delete"];
 
 var mask = function(input, in_mask, options) {
 	if (options) {
@@ -12,30 +13,37 @@ var mask = function(input, in_mask, options) {
     }
   }
 
-  if (in_mask.indexOf(rep_char) > -1) {
-  	options.first_rep = in_mask.indexOf(rep_char);
-    console.log("first_rep: " + options.first_rep);
-
-    if (in_mask.substring(options.first_rep+1).indexOf(rep_char) > -1) {
-    	options.second_rep = in_mask.substring(options.first_rep+1).indexOf(rep_char);
-      options.second_rep += options.first_rep+1;
-      console.log("second_rep: " + options.second_rep);
-
-      if (in_mask.substring(options.second_rep+1).indexOf(rep_char) > -1) {
-      	options.third_rep = in_mask.substring(options.second_rep+1).indexOf(rep_char);
-      	options.third_rep += options.second_rep+1;
-        console.log("third_rep: " + options.third_rep);
-      }
-    }
-  }
-
+  // Checks if c is a special character
   var isSpecialChar = function(c, next) {
     return ((next != escape_char) && (c === rep_char || c === alpha_char ||
            c === white_char || c === num_char || c === escape_char));
   };
 
+  // Checks if c is a delete character
+  var isDelete = function(c) {
+    return (deletes.indexOf(c) > -1);
+  };
+
+  // Adds literals if needed
+  var checkLiterals = function(e) {
+    var temp = "";
+    while (!isSpecialChar(in_mask[mask_cursor], in_mask[mask_cursor+1])) {
+      temp += in_mask[mask_cursor];
+      ++mask_cursor;
+    }
+
+    if (temp) {
+      e.target.value += temp;
+    }
+  };
+
+  // Holds the place in the input mask
   var mask_cursor = 0;
+  // The core functionality for the masking
   var keydown_listener = function(e) {
+    console.log("Down [" + mask_cursor + "]: " + in_mask[mask_cursor]);
+    var valid = false;
+
     if (isSpecialChar(in_mask[mask_cursor], in_mask[mask_cursor+1])) {
       switch (in_mask[mask_cursor]) {
         case rep_char:
@@ -49,6 +57,9 @@ var mask = function(input, in_mask, options) {
           break;
         case num_char:
           console.log("Number character");
+          if (e.key >= "0" && e.key <= "9") {
+            valid = true;
+          }
           break;
         case escape_char:
           console.log("Escape character");
@@ -56,14 +67,45 @@ var mask = function(input, in_mask, options) {
       }
     }
 
-    if (e.key != in_mask[mask_cursor]) {
+    if (!valid && !isDelete(e.key)) {
       e.preventDefault();
-    } else {
+    } else if (valid) {
       ++mask_cursor;
     }
   };
 
-  addEventListener("keydown", keydown_listener, {capture: true});
+  var keyup_listener = function(e) {
+    checkLiterals(e);
+    if (isDelete(e.key)) {
+      if (mask_cursor > 0) {
+        --mask_cursor;
+      }
+    }
+
+    console.log("Up [" + mask_cursor + "]: " + in_mask[mask_cursor]);
+  }
+
+  // Adds any literals to beginning of input
+  var focus_listener = function(e) {
+    checkLiterals(e);
+  };
+
+  // Sets input to default if focus is lost before valid input
+  var focus_lost_listener = function(e) {
+    if (options.min > 0 && e.target.value.length < options.min && e.target.value.length > 0) {
+      e.target.value = options.default;
+    }
+  };
+
+  input.addEventListener("keydown", keydown_listener);
+  input.addEventListener("keyup", keyup_listener);
+  input.addEventListener("focusin", focus_listener);
+  input.addEventListener("focusout", focus_lost_listener);
 };
 
-mask(document.getElementById('money'), "$###,?", {placeholder: "$0.00"});
+mask(document.getElementById('money'), "$###,?.##", {
+  placeholder: "$0.00",
+  min: 5,
+  default: "$0.00",
+  post: undefined, // will be function that runs on input value on focus lost
+});
